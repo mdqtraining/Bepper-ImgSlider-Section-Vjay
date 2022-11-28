1fe7f38fa20a9244254b34c7ff17cfb53682b4c3
Vijay-MDQ <vijayaprasath.mdq@gmail.com>
2022-11-28 14:54:55 +0530

    added responsive slider

---
+++ README.md
@@ -0,0 +1,2 @@
+# beppers
+ 
---
+++ use.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Document</title>
+  <style>
+   
+  </style>
+</head>
+<body>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block" width="300vw" height="500vh" src="img/images/start.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" width="300vw" height="500vh" src="img/images/login.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" width="300vw" height="500vh" src="img/images/search.jpeg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" width="300vw" height="500vh" src="img/images/business.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" width="300vw" height="500vh" src="img/images/services.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" width="300vw" height="500vh" src="img/images/contact.jpeg">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</body>
+</html>
